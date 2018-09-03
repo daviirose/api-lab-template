41ef2dab144dc69728acf7e814f9a14b94f4dda2
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-09-03 14:18:15 -0400

    Writing out code comments to make sure i understand whats going on

--- index.php
+++ index.php
@@ -4,3 +4,10 @@ use \Psr\Http\Message\ResponseInterface as Response; //This allows us to use the
 
 require './vendor/autoload.php'; //It allows you to automatically pull in scripts and classes without having to do it manually
 $app = new \Slim\App; //$app is our instance of slim.
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+//{name} in the url indicates what the argument will be called
+//Line 9; adds the text "Hello, $name" to the response
