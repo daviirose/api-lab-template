c130559b7a7ea5ac570c5cc14384349983c2afc2
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-09-05 08:09:55 -0400

    no message

---
+++ index.php
@@ -0,0 +1,12 @@
+$app->run(); //This tells PHP to run the slim app
+require './vendor/autoload.php';
+$db = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+  };
+$app = (new Davian\api\App($db))->get();
+$app->run();
