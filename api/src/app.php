41c24a4e572cf22b9c1096a13687c8d0c5cd2f8a
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-09-13 11:45:03 -0400

    no message

--- api/src/app.php
+++ api/src/app.php
@@ -39,7 +39,6 @@ class App
        $this->logger->addInfo("GET /cars");
        $cars = $this->db->query('SELECT * from cars')->fetchAll();
        $jsonResponse = $response->withJson($cars);
-       var_dump($cars);
        return $jsonResponse;
    });
    $app->get('/cars/{id}', function (Request $request, Response $response, array $args) {
@@ -77,6 +76,49 @@ class App
 
        return $jsonResponse;
    }); //Update cars by id
+$app->post('/cars', function (Request $request, Response $response) {
+
+     // check that peron exists
+     // $athlete = $this->db->query('SELECT * from cars where id='.$id)->fetch();
+     // if(!$athlete){
+     //   $errorData = array('status' => 404, 'message' => 'not found');
+     //   $response = $response->withJson($errorData, 404);
+     //   return $response;
+     // }
+
+     // build query string
+     $createString = "INSERT INTO cars ";
+     $fields = $request->getParsedBody();
+     $keysArray = array_keys($fields);
+     $last_key = end($keysArray);
+     $values = '(';
+     $fieldNames = '(';
+     foreach($fields as $field => $value) {
+       $values = $values . "'"."$value"."'";
+       $fieldNames = $fieldNames . "$field";
+       if ($field != $last_key) {
+         // conditionally add a comma to avoid sql syntax problems
+         $values = $values . ", ";
+         $fieldNames = $fieldNames . ", ";
+       }
+     }
+     $values = $values . ')';
+     $fieldNames = $fieldNames . ') VALUES ';
+     $createString = $createString . $fieldNames . $values . ";";
+     // execute query
+     try {
+       $this->db->exec($createString);
+     } catch (\PDOException $e) {
+       var_dump($e);
+       $errorData = array('status' => 400, 'message' => 'Invalid data provided to create athlete');
+       return $response->withJson($errorData, 400);
+     }
+     // return updated record
+     $athlete = $this->db->query('SELECT * from cars ORDER BY id desc LIMIT 1')->fetch();
+     $jsonResponse = $response->withJson($athlete);
+
+     return $jsonResponse;
+ });
 
    $app->delete('/cars/{id}', function (Request $request, Response $response, array $args) {
      $id = $args['id'];
